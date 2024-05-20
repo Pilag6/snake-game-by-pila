f866bc10c2cb6be094399cdbe9408f68e5dd1ec3
Pilag6 <pilag6@gmail.com>
2024-05-20 16:13:46 +0200

    chore: Update server port to use HTTPS URL

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = "https://snake-game-by-pila.onrender.com" || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
